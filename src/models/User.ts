ad60f2dbf973dd69a2c043e5a6043dba45d87fa6
Eoin O'Brien <eoinobrien910@gmail.com>
2017-08-14 22:36:32 +0100

    Remove template routes

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,5 +1,3 @@
-import * as bcrypt from 'bcrypt-nodejs';
-import * as crypto from 'crypto';
 import * as mongoose from 'mongoose';
 
 export type UserModel = mongoose.Document & {
@@ -17,9 +15,6 @@ export type UserModel = mongoose.Document & {
     website: string,
     picture: string,
   },
-
-  comparePassword: (candidatePassword: string, cb: (err: any, isMatch: any) => {}) => void,
-  gravatar: (size: number) => string,
 };
 
 export type AuthToken = {
@@ -46,42 +41,6 @@ const userSchema = new mongoose.Schema({
   },
 },                                     { timestamps: true });
 
-/**
- * Password hash middleware.
- */
-userSchema.pre('save', function save(next) {
-  const user = this;
-  if (!user.isModified('password')) { return next(); }
-  bcrypt.genSalt(10, (err, salt) => {
-    if (err) { return next(err); }
-    bcrypt.hash(user.password, salt, undefined, (err: mongoose.Error, hash) => {
-      if (err) { return next(err); }
-      user.password = hash;
-      next();
-    });
-  });
-});
-
-userSchema.methods.comparePassword = function (candidatePassword: string, cb: (err: any, isMatch: any) => {}) {
-  bcrypt.compare(candidatePassword, this.password, (err: mongoose.Error, isMatch: boolean) => {
-    cb(err, isMatch);
-  });
-};
-
-
-/**
- * Helper method for getting user's gravatar.
- */
-userSchema.methods.gravatar = function (size: number) {
-  if (!size) {
-    size = 200;
-  }
-  if (!this.email) {
-    return `https://gravatar.com/avatar/?s=${size}&d=retro`;
-  }
-  const md5 = crypto.createHash('md5').update(this.email).digest('hex');
-  return `https://gravatar.com/avatar/${md5}?s=${size}&d=retro`;
-};
 
 // export const User: UserType = mongoose.model<UserType>('User', userSchema);
 const User = mongoose.model('User', userSchema);
--- src/server.ts
+++ src/server.ts
@@ -10,9 +10,8 @@ import * as errorHandler from 'errorhandler';
 import * as lusca from 'lusca';
 import * as dotenv from 'dotenv';
 import * as mongo from 'connect-mongo';
-import * as path from 'path';
 import * as mongoose from 'mongoose';
-import expressValidator = require('express-validator');
+const expressValidator = require('express-validator');
 
 
 const mongoStoreFactory = mongo(session);
@@ -26,10 +25,7 @@ dotenv.config({ path: '.env.example' });
 /**
  * Controllers (route handlers).
  */
-import * as homeController from './controllers/home';
-import * as userController from './controllers/user';
 import * as apiController from './controllers/api';
-import * as contactController from './controllers/contact';
 
 /**
  * Create Express server.
@@ -69,44 +65,9 @@ app.use(session({
 }));
 app.use(lusca.xframe('SAMEORIGIN'));
 app.use(lusca.xssProtection(true));
-app.use((req, res, next) => {
-  res.locals.user = req.user;
-  next();
-});
-app.use((req, res, next) => {
-  // After successful login, redirect back to the intended page
-  if (!req.user &&
-      req.path !== '/login' &&
-      req.path !== '/signup' &&
-      !req.path.match(/^\/auth/) &&
-      !req.path.match(/\./)) {
-    req.session.returnTo = req.path;
-  } else if (req.user &&
-      req.path == '/account') {
-    req.session.returnTo = req.path;
-  }
-  next();
-});
-app.use(express.static(path.join(__dirname, 'public'), { maxAge: 31557600000 }));
 
 /**
- * Primary app routes.
- */
-app.get('/', homeController.index);
-app.get('/login', userController.getLogin);
-app.post('/login', userController.postLogin);
-app.get('/logout', userController.logout);
-app.get('/forgot', userController.getForgot);
-app.post('/forgot', userController.postForgot);
-app.get('/reset/:token', userController.getReset);
-app.post('/reset/:token', userController.postReset);
-app.get('/signup', userController.getSignup);
-app.post('/signup', userController.postSignup);
-app.get('/contact', contactController.getContact);
-app.post('/contact', contactController.postContact);
-
-/**
- * API examples routes.
+ * API routes.
  */
 app.get('/api', apiController.getApi);
 
