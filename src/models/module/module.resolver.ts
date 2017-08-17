f3786a0d15832bc43b308876b5b5041ac320121a
Eoin O'Brien <eoinobrien910@gmail.com>
2017-08-18 00:20:07 +0100

    Remove debug logging

--- src/models/module/module.resolver.ts
+++ src/models/module/module.resolver.ts
@@ -13,7 +13,6 @@ function getModule(_id: string): Promise<ModuleType> {
 export const resolvers = {
   RootQuery: {
     module(obj: Object, args: IModuleArgs) {
-      console.log(`Module resolver called for ${args._id}`);
       return getModule(args._id);
     },
   },
