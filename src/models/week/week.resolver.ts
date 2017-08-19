fb91fb70195e767acc926d9d27f4cc7c86b0f970
Eoin O'Brien <eoinobrien910@gmail.com>
2017-08-19 17:26:49 +0100

    Add ID validators for queries

--- src/models/week/week.resolver.ts
+++ src/models/week/week.resolver.ts
@@ -3,12 +3,20 @@ import { IWeek } from '../../types/models/IWeek';
 import { IDataLoaders } from '../../dataloaders';
 import { ILesson } from '../../types/models/ILesson';
 import { IWeekQueryArgs } from '../../types/query-args/IWeekQueryArgs';
+import { GraphQLError } from 'graphql';
+
+function assertValidWeekId(id: string) {
+  if (!/^[1-9][0-4]?$/.test(id)) {
+    throw new GraphQLError(`Week ID "${id}" is invalid.`);
+  }
+}
 
 export function getWeeks(): Promise<IWeek[]> {
   return scrapeWeeks();
 }
 
 export function getWeek(_id: string): Promise<IWeek> {
+  assertValidWeekId(_id);
   return getWeeks()
     .then(weeks => weeks.find(week => week._id === _id));
 }
