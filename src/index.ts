90793eb915d0c1ed1d161aed8c0111061d1e16f5
Eoin O'Brien <eoinobrien910@gmail.com>
2018-06-05 01:31:41 +0100

    feat(graphql): implement AWS Lambda DataSource function for AppSync

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,81 @@
-export const hello: AWSLambda.Handler = (event: AWSLambda.APIGatewayEvent, context: AWSLambda.Context, cb: AWSLambda.Callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
+import {
+  courseTimetableDataLoader,
+  loadFromDataLoader,
+  moduleDataLoader,
+  moduleExamTimetableDataLoader,
+  moduleTimetableDataLoader,
+  roomDataLoader,
+  roomTimetableDataLoader,
+  studentExamTimetableDataLoader,
+  studentTimetableDataLoader,
+  weekDataLoader,
+} from './dataloaders';
+import {
+  IAppSyncEvent,
+  ICourseTimetableBatchEvent,
+  IModuleBatchEvent,
+  IModuleExamTimetableBatchEvent,
+  IModuleTimetableBatchEvent,
+  IRoomBatchEvent,
+  IRoomTimetableBatchEvent,
+  IStudentExamTimetableBatchEvent,
+  IStudentTimetableBatchEvent,
+  IWeekBatchEvent,
+} from './types';
 
-  cb(null, response);
+export const graphqlHandler: AWSLambda.Handler = (event: IAppSyncEvent[], _: AWSLambda.Context, cb: AWSLambda.Callback) => {
+  const field = event[0].field;
+
+  switch (field) {
+    case 'courseTimetable':
+      loadFromDataLoader(courseTimetableDataLoader(), event as ICourseTimetableBatchEvent[], cb);
+      break;
+
+    case 'moduleTimetable':
+      loadFromDataLoader(moduleTimetableDataLoader(), event as IModuleTimetableBatchEvent[], cb);
+      break;
+
+    case 'roomTimetable':
+      loadFromDataLoader(roomTimetableDataLoader(), event as IRoomTimetableBatchEvent[], cb);
+      break;
+
+    case 'studentTimetable':
+      loadFromDataLoader(studentTimetableDataLoader(), event as IStudentTimetableBatchEvent[], cb);
+      break;
+
+    case 'moduleExamTimetable':
+      loadFromDataLoader(moduleExamTimetableDataLoader(), event as IModuleExamTimetableBatchEvent[], cb);
+      break;
+
+    case 'studentExamTimetable':
+      loadFromDataLoader(studentExamTimetableDataLoader(), event as IStudentExamTimetableBatchEvent[], cb);
+      break;
+
+    case 'module':
+    case 'CourseTimetableLesson.module':
+    case 'RoomTimetableLesson.modules':
+    case 'StudentTimetableLesson.module':
+    case 'ModuleExamTimetable.module':
+      loadFromDataLoader(moduleDataLoader(), event as IModuleBatchEvent[], cb);
+      break;
+
+    case 'room':
+    case 'CourseTimetableLesson.rooms':
+    case 'ModuleTimetableLesson.rooms':
+    case 'StudentTimetableLesson.rooms':
+    case 'ModuleExamTimetable.room':
+      loadFromDataLoader(roomDataLoader(), event as IRoomBatchEvent[], cb);
+      break;
+
+    case 'week':
+    case 'CourseTimetableLesson.weeks':
+    case 'ModuleTimetableLesson.weeks':
+    case 'RoomTimetableLesson.weeks':
+    case 'StudentTimetableLesson.weeks':
+      loadFromDataLoader(weekDataLoader(), event as IWeekBatchEvent[], cb);
+      break;
+
+    default:
+      cb(new Error(`Unknown field, unable to resolve "${field}"`));
+  }
 };
